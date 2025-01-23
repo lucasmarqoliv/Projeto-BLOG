2d21d68ea547570215d4ac86fdbe5d51261d5e82
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2025-01-23 08:09:59 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\SobreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts.index');
 });
 
 Route::get('post/listar', [PostController::class, 'index'])->name('post.index');
